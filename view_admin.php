4f1900bac7561a25525b2b801c563da4cd53d117
jonas-ofc <134335040+jonas-ofc@users.noreply.github.com>
2023-05-22 23:37:50 +0200

    Adding files from repos

---
+++ view_admin.php
@@ -0,0 +1,22 @@
+<?php 
+
+$_page = 'admin';
+/* $_css = 'style_login.css'; */
+
+require_once __DIR__.'/comp_header.php';
+
+?>
+
+<main>
+
+
+<h2>ADMIN</h2>
+
+</main>
+
+
+<?php 
+
+require_once __DIR__.'/comp_footer.php'
+
+?>
